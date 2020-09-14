1a5f3429b06ef6b284bf5b6b505089d856290aed
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-09-14 11:45:57 +0200

    WIP

--- packages/inertia-interceptor/src/interceptor.js
+++ packages/inertia-interceptor/src/interceptor.js
@@ -2,7 +2,7 @@ import { Inertia } from '@inertiajs/inertia'
 import { default as Route } from './route'
 
 export default {
-  setup(element = null, endpoint = '/', routes = null) {
+  setup(element = null, routes = null) {
     // When the user passes a closure, we'll call it with the Route helper.
     // This allows them to define routes without the need to manually
     // import the 'Route' helper in their codebase. Useful!
@@ -10,19 +10,18 @@ export default {
       routes(Route)
     }
 
-    if (element && Route.exists('GET', endpoint)) {
+    if (element && Route.exists('GET', window.location.href)) {
       // Next, we'll initialize the 'root' element that Inertia mounts on.
       // We'll do this by 'visiting' an initial page, and by 'passing'
       // the page data, just like the @inertia directive would do.
-      element.setAttribute('data-page', JSON.stringify(Route.invoke('GET', endpoint)))
+      element.setAttribute('data-page', JSON.stringify(Route.invoke('GET', window.location.href)))
     }
 
     // Finally, we register ourselves to Inertia's on-visit listener.
     // We'll use this to intercept & cancel the outgoing request
     // after which we'll resolve and update the page directly
     Inertia.on('visit', event => {
-      const rootAddress = window.location.href.split('/').slice(0, 3).join('/')
-      const path = event.detail.url.toString().replace(rootAddress, '').split('?')[0]
+      const path = event.detail.url.toString()
       const method = event.detail.options.method || 'GET'
 
       // Check if the route is actually registered.
--- packages/inertia-interceptor/src/route.js
+++ packages/inertia-interceptor/src/route.js
@@ -1,59 +1,68 @@
 export default {
   routes: {},
-  ensurePathPrefixed(path) {
-    return path.charAt(0) !== '/'
-      ? '/' + path
-      : path
+  parseRoute(uri) {
+    const rootAddress = window.location.href.split('/').slice(0, 3).join('/')
+    const [ endpoint, params ] = uri.replace(rootAddress, '').split('?')
+
+    return {
+      route: endpoint && endpoint.charAt(0) === '/' ? endpoint : '/' + endpoint,
+      params: new URLSearchParams(params || {}),
+    }
   },
-  register(method, path, callback = null) {
+  addRoute(method, uri, callback = null) {
     const _method = method.toLowerCase()
+    const { route } = this.parseRoute(uri)
 
     // Ensure that the method 'group' (GET, POST, PATCH) exists.
     if (Object.keys(this.routes).indexOf(_method) === -1) {
       this.routes[_method] = {}
     }
 
-    this.routes[_method][this.ensurePathPrefixed(path)] = callback
+    this.routes[_method][route] = callback
   },
-  exists(method, path) {
-    if (Object.keys(this.routes[method.toLowerCase()] || {}).indexOf(this.ensurePathPrefixed(path)) > -1) {
+  exists(method, uri) {
+    const { route } = this.parseRoute(uri)
+    if (Object.keys(this.routes[method.toLowerCase()] || {}).indexOf(route) > -1) {
       return true
     }
 
-    console.error(`[Interceptor] No route registered for [${method.toUpperCase()} ${path}].`, this.routes)
+    console.error(`[Interceptor] No route registered for [${method.toUpperCase()} ${route}].`, this.routes)
     return false
   },
-  invoke(method, path, event = {}) {
-    const route = this.routes[method.toLowerCase()][this.ensurePathPrefixed(path)]
-    if (typeof route !== 'function') {
-      console.error(`[Interceptor] Route [${method.toUpperCase()} ${path}] is not a function.`)
+  invoke(method, uri, event = null) {
+    const { route, params } = this.parseRoute(uri)
+
+    const action = this.routes[method.toLowerCase()][route]
+    if (typeof action !== 'function') {
+      console.error(`[Interceptor] Route [${method.toUpperCase()} ${route}] is not a function.`)
 
       return false
     }
 
-    const response = route(event)
+    const response = action({ params, uri, event })
 
     // After all redirects are said and done, we'll make sure to
-    // set the current 'url' to the first response's route.
+    // set our resulting url to the current URI so it can be
+    // properly reflected in the browser's address bar.
     if (response.url === null) {
-      response.url = path
+      response.url = uri
     }
 
     return response
   },
   get(path, callback = null) {
-    return this.register('GET', path, callback)
+    return this.addRoute('GET', path, callback)
   },
   post(path, callback = null) {
-    return this.register('POST', path, callback)
+    return this.addRoute('POST', path, callback)
   },
   put(path, callback = null) {
-    return this.register('PUT', path, callback)
+    return this.addRoute('PUT', path, callback)
   },
   patch(path, callback = null) {
-    return this.register('PATCH', path, callback)
+    return this.addRoute('PATCH', path, callback)
   },
   delete(path, callback = null) {
-    return this.register('DELETE', path, callback)
+    return this.addRoute('DELETE', path, callback)
   },
 }
